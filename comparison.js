b3aa95c9edc58a6469d6f7283ef6d8e00453cde4
Masud Rana <mr007masudrana@gmail.com>
2025-04-14 14:36:21 +0600

    first commit

---
+++ comparison.js
@@ -0,0 +1,58 @@
+//*********Greater than: >  ***********
+
+//5 is greater than 10
+
+console.log(5 > 10); // it is false/wrong
+
+//8 is greater than 6
+
+console.log(8 > 6); // it is true/right
+
+//*********Less than: <  ***********
+
+//2 is less than 4
+console.log(2 < 4); // it is true/right
+
+//3 is less than 2 
+console.log(3 < 2);  // it is false/wrong
+
+//*********Equal: ==  ***********
+
+//7 is equal to 8 
+
+console.log(7 == 8); // it is false/wrong
+
+//7 is equal to 7 
+
+console.log(7 == 7);  //it is true/right
+
+//********* Greater Than or Equal: >=  ***********
+
+//is 10 greater than or equal to 10
+console.log(10 >= 10);
+
+//is 12 greater than or equal to 13
+console.log(12 >= 13);  // it is false/wrong
+
+//is 13 greater than or equal to 12
+console.log(13 >= 12);
+
+//********* Less Than or Equal: <=  ***********
+
+//is 15 Less than or equal to 16
+
+console.log(15 <= 16); //it is true/right
+
+//is 16 Less than or equal to 15
+console.log(16 <= 15); //it is false/wrong
+
+//is 16 Less than or equal to 16
+console.log(16 <= 16); //it is true/right
+
+//********* Not Equal: !=  ***********
+
+//is 20 not equal to 22
+console.log(20 != 22); //it is true/right
+
+//is 20 not equal to 20
+console.log(20 != 20); //it is false/wrong
